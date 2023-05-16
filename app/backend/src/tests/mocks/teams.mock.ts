f83ded93f7eed6d80103dac271f6fd5d10ddc7f0
D-Wolter <wolter.daniel@gmail.com>
2023-05-16 17:18:18 -0300

    feat: refatoração dos testes de /TEAMS

--- app/backend/src/tests/mocks/teams.mock.ts
+++ app/backend/src/tests/mocks/teams.mock.ts
@@ -1,9 +1,8 @@
-const teams = [
+const allTeams = [
     { "id": 1, "teamName": "Avaí/Kindermann" },
     { "id": 2, "teamName": "Bahia" },
     { "id": 3, "teamName": "Botafogo" },
 ];
 
-const teamId = { "id": 5, "teamName": "Cruzeiro" }
 
-export default { teams, teamId };
+export default allTeams;
--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -1,66 +1,50 @@
-import * as sinon from 'sinon';
-import * as chai from 'chai';
-import { App } from '../app';
-import teamsMocks from './mocks/teams.mock';
-import { Response } from 'superagent';
-import Team from '../database/models/TeamModel';
-
+import * as sinon from "sinon";
+import * as chai from "chai";
 // @ts-ignore
-import chaiHttp = require('chai-http');
-
+import chaiHttp = require("chai-http");
+import Teams from "../database/models/TeamModel";
+import { Response } from "superagent";
+import { Model } from "sequelize";
+import { app } from "../app";
+import allTeams from "./mocks/teams.mock";
 chai.use(chaiHttp);
-
-const { app } = new App();
 const { expect } = chai;
 
-describe('Testes do ENDPOINT /teams', () => {
-    let chaiHttpResponse: Response;
+describe("Testa a rota de Teams", function () {
+  let chaiHttpResponse: Response;
+  afterEach(() => {
+    sinon.restore();
+  });
+  describe("Testes do ENDPOINT /teams", function () {
+    it("se retorna todos os times com getAllTeams no ENDPOINT /teams", async function () {
+      sinon.stub(Model, "findAll").resolves(allTeams as Teams[]);
 
-    it('se retorna todos os times com getAllTeams no ENDPOINT /teams', async () => {
-        sinon
-        .stub(Team, "findAll")
-        .resolves(teamsMocks.teams as Team[]);
+      chaiHttpResponse = await chai.request(app).get("/teams");
 
-        chaiHttpResponse = await chai
-        .request(app)
-        .get('/teams')
+      expect(chaiHttpResponse.status).to.be.equal(200);
 
-        expect(chaiHttpResponse).to.have.status(200);
-        expect(chaiHttpResponse.body).to.be.an('array');
-        expect(chaiHttpResponse.body).to.be.deep.equal(teamsMocks.teams);
+      expect(chaiHttpResponse.body).to.deep.equal(allTeams);
     });
+  });
 
-    it('se retorna de 1 time com getTeamById no ENDPOINT /teams/:id', async () => {
-        const teamId = 5;
+  describe("Testes do ENDPOINT /teams/:id", function () {
+    sinon.stub(Model, "findByPk").resolves(allTeams[1] as Teams);
+    it("se retorna de 1 time com getTeamById no ENDPOINT /teams/:id", async function () {
+      chaiHttpResponse = await chai.request(app).get("/teams/2");
 
-        sinon
-        .stub(Team, "findByPk")
-        .resolves(teamsMocks.teamId as Team);
+      expect(chaiHttpResponse.status).to.be.equal(200);
 
-        chaiHttpResponse = await chai
-        .request(app)
-        .get(`/teams/${teamId}`)
-
-        expect(chaiHttpResponse).to.have.status(200);
-        expect(chaiHttpResponse.body).to.be.an('object');
-        expect(chaiHttpResponse.body).to.be.deep.equal(teamsMocks.teamId);
-        (Team.findByPk as sinon.SinonStub).restore();
+      expect(chaiHttpResponse.body).to.deep.equal(allTeams[1]);
     });
+  });
+  describe("Testes do ENDPOINT /teams/:id", function () {
+    it("se no ENDPOINT teams/id com id inválido", async function () {
+        sinon.stub(Model, 'findByPk').resolves(null)
+        const httpRes = await chai.request(app).get('/teams/999');
+        expect(httpRes.status).to.be.equal(200);
+        expect(httpRes.body).null;
+      });
 
-    it('se retorna erro chamando no ENDPOINT /teams/:id getTeamById com id inválido', async () => {
-        const teamId = 99;
-
-        sinon
-        .stub(Team, "findByPk")
-        .resolves(null);
-
-        chaiHttpResponse = await chai
-        .request(app)
-        .get(`/teams/${teamId}`)
-
-        expect(chaiHttpResponse).to.have.status(200);
-        expect(chaiHttpResponse.body).to.be.null;
-    
-        (Team.findByPk as sinon.SinonStub).restore();
     });
+  
 });
